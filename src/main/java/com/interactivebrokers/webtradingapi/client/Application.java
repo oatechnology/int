cd7393eb54835735f23959b9933764a27907a311
unknown <oatechnology256@gmail.com>
2018-03-04 03:13:08 +0200

    remove servlets from Application

--- src/main/java/com/interactivebrokers/webtradingapi/client/Application.java
+++ src/main/java/com/interactivebrokers/webtradingapi/client/Application.java
@@ -9,17 +9,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration 
+
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-
+public class Application {
+ 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
