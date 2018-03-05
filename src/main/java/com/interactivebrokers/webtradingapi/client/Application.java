449d7d55a3cb3259720eb3bc0d81901fd7e6b366
Osama Ali <oatechnology256@gmail.com>
2018-03-05 23:50:59 +0200

    update application.java

--- src/main/java/com/interactivebrokers/webtradingapi/client/Application.java
+++ src/main/java/com/interactivebrokers/webtradingapi/client/Application.java
@@ -7,11 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import bootwildfly.client.Application;
-
-
-
+ 
 @SpringBootApplication
 public class Application  extends SpringBootServletInitializer {
  
